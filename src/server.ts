effbbc970ea800a1b0dcce1a3fb13cf9c452b06a
Eder Queiroz <62737156+Eder-Queiroz@users.noreply.github.com>
2023-11-24 09:08:31 -0300

    Generate pdf

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,10 @@ import cors from "cors";
 import dotenv from "dotenv";
 import { router } from "./router";
 import AlgoritmoGenetico from "./Core/AlgoritmoGenetico";
+import pdfMake from "pdfmake/build/pdfmake";
+import pdfFonts from "pdfmake/build/vfs_fonts";
+import { TDocumentDefinitions } from "pdfmake/interfaces";
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 dotenv.config();
 const { PORT } = process.env;
@@ -29,15 +33,27 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   });
 });
 
-app.get("/ag", (req, res) => {
+app.get("/ag", async (req, res) => {
   const ag = new AlgoritmoGenetico();
-  const result = ag.ngOnInit();
-  return res.json(result);
-})
+  const result = await ag.ngOnInit();
+
+  const pdfDoc = pdfMake.createPdf(result as unknown as TDocumentDefinitions);
+
+  //baixar o pdf criado em pdfDoc
+
+  pdfDoc.getBase64((data) => {
+    res.writeHead(200, {
+      "Content-Type": "application/pdf",
+      "Content-Disposition": 'attachment;filename="GradeHoraria.pdf"',
+    });
+
+    const download = Buffer.from(data.toString(), "base64");
+    res.end(download);
+  });
+});
 
 app.listen(PORT, () =>
   console.log(
     `Server online!\nServidor rodando na porta ${PORT} => http://localhost:${PORT}`
   )
 );
-
